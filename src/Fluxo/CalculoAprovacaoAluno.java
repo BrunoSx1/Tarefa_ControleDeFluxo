91e7abf01cbd13f7e5d03362ea44861ef94264af
Bruno Silva <s.bruno.12@hotmail.com>
2023-11-08 05:10:40 -0300

    first commit

---
+++ src/Fluxo/CalculoAprovacaoAluno.java
@@ -0,0 +1,33 @@
+package Fluxo;
+import java.util.Scanner;
+
+public class CalculoAprovacaoAluno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        double nota1, nota2, nota3, nota4;
+        System.out.print("Digite a primeira nota: ");
+        nota1 = scanner.nextDouble();
+        System.out.print("Digite a segunda nota: ");
+        nota2 = scanner.nextDouble();
+        System.out.print("Digite a terceira nota: ");
+        nota3 = scanner.nextDouble();
+        System.out.print("Digite a quarta nota: ");
+        nota4 = scanner.nextDouble();
+
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+
+        if (media >= 7.0) {
+            System.out.println("Aluno aprovado!");
+        } else if (media >= 5.0) {
+            System.out.println("Aluno em recuperação.");
+        } else {
+            System.out.println("Aluno reprovado.");
+        }
+
+        scanner.close();
+    }
+}
